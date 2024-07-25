79cdb4639c900571f84f102c7c6221da76d26c45
ignacio <ign.garcia61@gmail.com>
2024-07-25 20:51:21 -0300

    chore: config env variables

--- backend/src/main/java/com/garcia/letsgrind/LetsgrindApplication.java
+++ backend/src/main/java/com/garcia/letsgrind/LetsgrindApplication.java
@@ -1,7 +1,7 @@
 package com.garcia.letsgrind;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 
 @SpringBootApplication
 public class LetsgrindApplication {
